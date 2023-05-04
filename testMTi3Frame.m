79f32d2ea9fdd8b77fe7ee0e42e4f57bd1297560
haozheng huang <huang.haozheng@outlook.com>
2023-05-05 00:11:52 +0200

    so, the sensor frame is rotated from the original board (body) frame probably due to disabling the magnetometer

--- testMTi3Frame.m
+++ testMTi3Frame.m
@@ -56,9 +56,8 @@ psi0 = deg2rad(360 - 324.37);  % yaw at 26.0 s
 theta0 = 0;  % pitch
 phi0 = 0;  % roll
 
-% MTi-3 frame is rotated about z-axis by -88.75 (or -59) deg (probably due to disabling the magnetometer)
-R_board_sensor = R3(deg2rad(-59));  % MTi-3 board frame to sensor frame
-Rnb0 = R3(0.5 * pi) * R1(pi);  % n-frame to MTi-3 board frame (body frame)
+% MTi-3 frame is rotated about z-axis by -88.75 deg (probably due to disabling the magnetometer)
+Rnb0 = R3(0.5 * pi) * R1(pi) * R3(deg2rad(-88.75));  % n-frame to MTi-3 frame
 Rb0b = R3(psi0)*R2(theta0)*R1(phi0);  % align yaw of MTi-3 frame to actual heading
 Rnb = Rnb0 * Rb0b;  % n-frame to b-frame
 
@@ -97,71 +96,77 @@ LOG.Rnb(1:range_end-range_start+1, 1:3) = nan;
 LOG.Rb0b(1:range_end-range_start+1, 1:3) = nan;
 LOG.llh_incre_total(1:range_end-range_start+1, 1:3) = nan;
 LOG.llh_incre(1:range_end-range_start+1, 1:3) = nan;
+LOG.x(1:range_end-range_start+1) = nan;
+LOG.y(1:range_end-range_start+1) = nan;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%% PAY ATTENTION TO ORIENTATION OF THE BODY FRAME %%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-lat_incre_total = 0;
-lon_incre_total = 0;
-lat = lat0;
-lon = lon0;
-lat0_int = single(floor(lat0));
-lon0_int = single(floor(lon0));
-lat0_frac = lat0 - lat0_int;
-lon0_frac = lon0 - lon0_int;
-
-h = h0;
-
+% lat_incre_total = 0;
+% lon_incre_total = 0;
+% lat = lat0;
+% lon = lon0;
+% lat0_int = single(floor(lat0));
+% lon0_int = single(floor(lon0));
+% lat0_frac = lat0 - lat0_int;
+% lon0_frac = lon0 - lon0_int;
+% 
+% h = h0;
+x = 0;
+y = 0;
+R0 = R3(deg2rad(-35.63-59));  % start at 26.0 s
+% R0 = R3(deg2rad(150.16-59));  % start at 67.0 s
 while k <= range_end
-    % read data from IMU
+    % % read data from IMU
     % w_ibx_b = gyrox(k);
     % w_iby_b = gyroy(k);
     % w_ibz_b = gyroz(k);
-    w_ib_b = R_board_sensor * [gyrox(k); gyroy(k); gyroz(k)];
-    w_ibx_b = w_ib_b(1);
-    w_iby_b = w_ib_b(2);
-    w_ibz_b = w_ib_b(3);
     % note that fAcc are already compensated by sensor with gravity
-    f_ib_b = R_board_sensor * [faccx(k); faccy(k); faccz(k)];
+    f_ib_b = [faccx(k); faccy(k); faccz(k)];
     % f_ib_b = [accx(k); accy(k); accz(k)];
 
-    % rotation
-    Rnb_ = Rnb;
-    omega_ib_b = [0, -w_ibz_b, w_iby_b;
-                  w_ibz_b, 0, -w_ibx_b;
-                  -w_iby_b, w_ibx_b, 0];
-    omega_ie_n_ = omega_ie_n;
-    Rnb = Rnb_ * (eye(3) + omega_ib_b * T) - omega_ie_n_ * Rnb_ * T;
+    % % rotation
+    % Rnb_ = Rnb;
+    % omega_ib_b = [0, -w_ibz_b, w_iby_b;
+    %               w_ibz_b, 0, -w_ibx_b;
+    %               -w_iby_b, w_ibx_b, 0];
+    % omega_ie_n_ = omega_ie_n;
+    % Rnb = Rnb_ * (eye(3) + omega_ib_b * T) - omega_ie_n_ * Rnb_ * T;
 
     % velocity
     v_eb_n_ = v_eb_n;
     % v_eb_n = v_eb_n_ + (Rnb_ * f_ib_b + g - cross(2 * Ren' * w_ie, v_eb_n)) * T;
     % v_eb_n = v_eb_n_ + (Rnb_ * f_ib_b + g) * T;
-    v_eb_n = v_eb_n_ + (Rnb_ * f_ib_b) * T;
+    % v_eb_n = v_eb_n_ + (Rnb_ * f_ib_b) * T;
+    v_eb_n = v_eb_n_ + R0 * f_ib_b * T;
     v_eb_n(3) = 0;  % disable velocity in Down direction
 
     % position
+    x = x + 0.5 * (v_eb_n_(1) + v_eb_n(1)) * T;
+    y = y + 0.5 * (v_eb_n_(2) + v_eb_n(2)) * T;
     % h = h;
-    lat_ = lat;
-    lat_incre = (v_eb_n_(1) / (meridionalRadius(lat_ / llh_scale) + h) + v_eb_n(1) / (meridionalRadius(lat_ / llh_scale) + h))...
-                * 0.5 * T * llh_scale;
-    lat_incre_total = lat_incre_total + lat_incre;
-    lat = lat0 + lat_incre_total;
-
-    lon_incre = (v_eb_n_(2) / ((transverseRadius(lat_ / llh_scale) + h) * cos(lat_ / llh_scale)) + v_eb_n(2) / ((transverseRadius(lat / llh_scale) + h) * cos(lat / llh_scale)))...
-                * 0.5 * T * llh_scale;
-    lon_incre_total = lon_incre_total + lon_incre;
-    lon = lon0 + lon_incre_total;
+    % lat_ = lat;
+    % lat_incre = (v_eb_n_(1) / (meridionalRadius(lat_ / llh_scale) + h) + v_eb_n(1) / (meridionalRadius(lat_ / llh_scale) + h))...
+    %             * 0.5 * T * llh_scale;
+    % lat_incre_total = lat_incre_total + lat_incre;
+    % lat = lat0 + lat_incre_total;
+    % 
+    % lon_incre = (v_eb_n_(2) / ((transverseRadius(lat_ / llh_scale) + h) * cos(lat_ / llh_scale)) + v_eb_n(2) / ((transverseRadius(lat / llh_scale) + h) * cos(lat / llh_scale)))...
+    %             * 0.5 * T * llh_scale;
+    % lon_incre_total = lon_incre_total + lon_incre;
+    % lon = lon0 + lon_incre_total;
 
     % logging
-    eul = rotm2eul(Rnb) * 180 / pi;
-    LOG.Rnb(k-range_start+1, :) = eul;
-    LOG.Rb0b(k-range_start+1, :) = rotm2eul(Rnb0'*Rnb) * 180 / pi;
+    % eul = rotm2eul(Rnb) * 180 / pi;
+    % LOG.Rnb(k-range_start+1, :) = eul;
+    % LOG.Rb0b(k-range_start+1, :) = rotm2eul(Rnb0'*Rnb) * 180 / pi;
     LOG.v_eb_n(k-range_start+1, :) = v_eb_n;
-    LOG.llh(k-range_start+1, :) = [lat, lon, h];
-    LOG.llh_incre_total(k-range_start+1, :) = [lat_incre_total, lon_incre_total, 0];
-    LOG.llh_incre(k-range_start+1, :) = [lat_incre, lon_incre, 0];
+    LOG.x(k-range_start+1) = x;
+    LOG.y(k-range_start+1) = y;
+    % LOG.llh(k-range_start+1, :) = [lat, lon, h];
+    % LOG.llh_incre_total(k-range_start+1, :) = [lat_incre_total, lon_incre_total, 0];
+    % LOG.llh_incre(k-range_start+1, :) = [lat_incre, lon_incre, 0];
 
     % increment step
     k = k + 1;
@@ -171,44 +176,58 @@ end
 %% plot
 close all;
 
-subplot(4,1,1);
-plot(t(data_range), LOG.Rb0b(:, 1), t(data_range), Yaw(data_range) + YawOffset,...
-     t(data_range), 360-heading(data_range))
-title('yaw')
-legend('INS-yaw', 'sensor-yaw')
+subplot(2, 1, 1)
 grid on
 hold on
+plot(t(data_range), LOG.v_eb_n(:, 1))
+plot(t(data_range), LOG.v_eb_n(:, 2))
+legend('v_eb_n(:, 1)', 'v_eb_n(:, 2)')
+
+subplot(2, 1, 2)
+grid on
+hold on
+plot(t(data_range), LOG.x)
+plot(t(data_range), LOG.y)
+legend('x', 'y')
 
 % subplot(4,1,1);
-% plot(t(data_range), LOG.Rnb(:, 1), t(data_range), Yaw(data_range) + YawOffset,...
+% plot(t(data_range), LOG.Rb0b(:, 1), t(data_range), Yaw(data_range) + YawOffset,...
 %      t(data_range), 360-heading(data_range))
 % title('yaw')
 % legend('INS-yaw', 'sensor-yaw')
 % grid on
 % hold on
-
-subplot(4,1,2);
-plot(t(data_range), LOG.v_eb_n(:, 1), '.', t(data_range), LOG.v_eb_n(:, 2), '.',...
-     t(data_range), VeloN(data_range), t(data_range), VeloE(data_range),...
-     t(data_range), LOG.v_eb_n(:, 3))
-title('velocity')
-legend('Vn INS', 'Ve INS', 'Vn MTi7', 'Ve MTi7', 'Vh')
-grid on
-hold on
-
-subplot(4,1,3);
-plot(t(data_range), LOG.llh_incre_total(:,1) + lat0_frac)
-title('position')
-legend('lat frac (milli rad)')
-grid on
-hold on
-
-subplot(4,1,4);
-plot(t(data_range), LOG.llh_incre_total(:,2) + lon0_frac)
-title('position')
-legend('lon frac (milli rad)')
-grid on
-hold on
+% 
+% % subplot(4,1,1);
+% % plot(t(data_range), LOG.Rnb(:, 1), t(data_range), Yaw(data_range) + YawOffset,...
+% %      t(data_range), 360-heading(data_range))
+% % title('yaw')
+% % legend('INS-yaw', 'sensor-yaw')
+% % grid on
+% % hold on
+% 
+% subplot(4,1,2);
+% plot(t(data_range), LOG.v_eb_n(:, 1), '.', t(data_range), LOG.v_eb_n(:, 2), '.',...
+%      t(data_range), VeloN(data_range), t(data_range), VeloE(data_range),...
+%      t(data_range), LOG.v_eb_n(:, 3))
+% title('velocity')
+% legend('Vn INS', 'Ve INS', 'Vn MTi7', 'Ve MTi7', 'Vh')
+% grid on
+% hold on
+% 
+% subplot(4,1,3);
+% plot(t(data_range), LOG.llh_incre_total(:,1) + lat0_frac)
+% title('position')
+% legend('lat frac (milli rad)')
+% grid on
+% hold on
+% 
+% subplot(4,1,4);
+% plot(t(data_range), LOG.llh_incre_total(:,2) + lon0_frac)
+% title('position')
+% legend('lon frac (milli rad)')
+% grid on
+% hold on
 
 % subplot(4,1,3);
 % plot(t(data_range), LOG.llh_incre_total(:,1) + lat0_frac - (lat_GNSS(data_range) * llh_scale - lat0_int))
