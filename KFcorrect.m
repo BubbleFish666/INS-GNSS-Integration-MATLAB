37acc9b2ddf8c4250a6e1429e2da8d8e1285d11a
haozheng huang <huang.haozheng@outlook.com>
2023-06-27 16:45:19 +0200

    adapt KFcorrect.m notation to introduction to Kalman Filter

--- KFcorrect.m
+++ KFcorrect.m
@@ -2,34 +2,34 @@
 % calculate measurement y with only fractional parts to avoid numerical issue
 KF.lat_GNSS_frac = GNSS.lat_GNSS(k) * llh_scale - INS.lat0_int;
 KF.lon_GNSS_frac = GNSS.lon_GNSS(k) * llh_scale - INS.lon0_int;
-% KF.y = [KF.lat_GNSS_frac; KF.lon_GNSS_frac]...
+% KF.z = [KF.lat_GNSS_frac; KF.lon_GNSS_frac]...
 %      - [INS.lat0_frac + INS.lat_incre_total;
 %         INS.lon0_frac + INS.lon_incre_total];  % milli rad
-KF.y = [GNSS.vn_GNSS(k); GNSS.ve_GNSS(k); KF.lat_GNSS_frac; KF.lon_GNSS_frac]...
+KF.z = [GNSS.vn_GNSS(k); GNSS.ve_GNSS(k); KF.lat_GNSS_frac; KF.lon_GNSS_frac]...
      - [INS.v_eb_n(1);
         INS.v_eb_n(2);
         INS.lat0_frac + INS.lat_incre_total;
         INS.lon0_frac + INS.lon_incre_total];  % milli rad
 
-KF.z_pre = [KF.dpsi_nb; KF.dv_eb_n; KF.dllh; KF.ba; KF.bg];
+KF.x_pre = [KF.dpsi_nb; KF.dv_eb_n; KF.dllh; KF.ba; KF.bg];
 
 % covariance of innovation
 KF.S = KF.H * KF.P * KF.H' + KF.R;
 % Kalman Gain
 KF.K = KF.P * KF.H' * KF.S^(-1);
-% update z
-KF.z = KF.z_pre + KF.K * (KF.y - KF.H * KF.z_pre);
+% update x
+KF.x = KF.x_pre + KF.K * (KF.z - KF.H * KF.x_pre);
 
-KF.dpsi_nb = KF.z(1:3);
+KF.dpsi_nb = KF.x(1:3);
 KF.Rnn = R3(KF.dpsi_nb(3)) * R2(KF.dpsi_nb(2)) * R1(KF.dpsi_nb(1));
-% KF.dv_eb_n = KF.z(4:6);
-% KF.dllh = KF.z(7:9);
-% KF.ba = KF.z(10:12);
-% KF.bg = KF.z(13:15);
-KF.dv_eb_n = KF.z(4:5);
-KF.dllh = KF.z(6:7);
-KF.ba = KF.z(8:10);
-KF.bg = KF.z(11:13);
+% KF.dv_eb_n = KF.x(4:6);
+% KF.dllh = KF.x(7:9);
+% KF.ba = KF.x(10:12);
+% KF.bg = KF.x(13:15);
+KF.dv_eb_n = KF.x(4:5);
+KF.dllh = KF.x(6:7);
+KF.ba = KF.x(8:10);
+KF.bg = KF.x(11:13);
 
 % dpsi_nb = [0; 0; 0];  % attitude error
 % dv_eb_n = [0; 0; 0];  % velocity error
