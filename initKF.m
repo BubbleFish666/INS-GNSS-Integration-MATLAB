bf491ecaa5c76ae70b862fdca76ffe31550ac870
haozheng huang <huang.haozheng@outlook.com>
2023-07-04 22:28:06 +0200

    comments in initIMUModel.m; correct initKF measurement noise

--- initKF.m
+++ initKF.m
@@ -36,7 +36,7 @@ KF.P(3,3) = deg2rad(10)^2;  % yaw variance
 KF.P(4,4) = 0.5^2;  % north velocity variance (1.0 m/s)^2
 KF.P(5,5) = 0.5^2;  % east velocity variance (1.0 m/s)^2
 KF.P(6,6) = (2.5 * llh_scale / meridionalRadius(INS.lat0 / llh_scale))^2;  % GNSS horizontal accuracy 2m -> lat (milli rad)^2
-KF.P(7,7) = (4.0 * llh_scale / transverseRadius(INS.lat0 / llh_scale) / cos(INS.lat0 / llh_scale))^2;  % GNSS horizontal accuracy 2m -> lon (milli rad)^2
+KF.P(7,7) = (2.5 * llh_scale / transverseRadius(INS.lat0 / llh_scale) / cos(INS.lat0 / llh_scale))^2;  % GNSS horizontal accuracy 2m -> lon (milli rad)^2
 KF.P(8,8) = (0.04e-3 * 10)^2;  % accelorometer bias (0.03 mg)^2
 KF.P(9,9) = (0.04e-3 * 10)^2;  % accelorometer bias (0.03 mg)^2
 KF.P(10,10) = (0.04 * 10 * 1e-3)^2;  % accelorometer bias (0.03 mg)^2
@@ -68,4 +68,5 @@ KF.Q(11:13, 11:13) = (3 * deg2rad(10) / 3600)^2 * eye(3);  % gyro dynamic bias 1
 KF.R = single([0.05^2, 0, 0, 0;
         0, 0.05^2, 0, 0;
         0, 0, (2.5 * llh_scale / meridionalRadius(INS.lat / llh_scale))^2, 0;
-        0, 0, 0, (4.0 * llh_scale / transverseRadius(INS.lat / llh_scale) / cos(INS.lat / llh_scale))^2]);
+        0, 0, 0, (2.5 * llh_scale / transverseRadius(INS.lat / llh_scale) / cos(INS.lat / llh_scale))^2]);
+        % 0, 0, 0, (2.5 * llh_scale / meridionalRadius(INS.lat / llh_scale))^2]);
